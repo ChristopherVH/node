44cc8d7fe08c27c6411625827ae5740e7db904d5
Christopher Van Hoosier <m712649@a4441145.federated.fds>
2017-03-06 10:37:03 -0800

    added status code

--- server.js
+++ server.js
@@ -475,9 +475,6 @@ app.post('/webhook', function (req, res) {
     // will time out and we will keep trying to resend.
     res.sendStatus(200);
    }
-   else{
-     res.sendStatus(400);
-   }
 });
 
 /*
